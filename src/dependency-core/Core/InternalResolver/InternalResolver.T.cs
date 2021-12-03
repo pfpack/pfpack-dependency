6e5483a52f3604c23b18f9751ba01ae93ac34b97
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-12-03 21:55:01 +0300

    Implement InternalResolver / the factories

---
+++ src/dependency-core/Core/InternalResolver/InternalResolver.T.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PrimeFuncPack;
+
+internal sealed class InternalResolver<TDependency>
+{
+    private readonly bool isDependency;
+
+    private readonly TDependency? dependency;
+
+    private readonly Func<IServiceProvider, TDependency>? resolver;
+
+    internal InternalResolver(TDependency dependency)
+        =>
+        (this.dependency, isDependency) = (dependency, true);
+
+    internal InternalResolver(Func<IServiceProvider, TDependency> resolver)
+        =>
+        this.resolver = resolver;
+
+    internal TDependency Invoke(IServiceProvider serviceProvider)
+        =>
+        IsDependency ? dependency : resolver.Invoke(serviceProvider);
+
+    [MemberNotNullWhen(true, nameof(dependency))]
+    [MemberNotNullWhen(false, nameof(resolver))]
+    private bool IsDependency => isDependency;
+}
---
+++ src/dependency-core/Core/InternalResolver/InternalResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimeFuncPack;
+
+internal static class InternalResolver
+{
+    public static InternalResolver<TDependency> Of<TDependency>(TDependency dependency)
+        =>
+        new(dependency);
+
+    public static InternalResolver<TDependency> From<TDependency>(Func<IServiceProvider, TDependency> resolver)
+        =>
+        new(resolver);
+}

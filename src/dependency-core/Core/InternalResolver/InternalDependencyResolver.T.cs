dc0a848f83ec139867f7a0ee2c108e3d5bf7586f
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-02-27 17:37:56 +0300

    InternalDependencyResolver: Minor refactor

--- src/dependency-core/Core/InternalResolver/InternalDependencyResolver.T.cs
+++ src/dependency-core/Core/InternalResolver/InternalDependencyResolver.T.cs
@@ -12,17 +12,35 @@ internal sealed class InternalDependencyResolver<TDependency>
 
     private readonly Func<IServiceProvider, TDependency>? resolver;
 
-    public InternalDependencyResolver(TDependency instance)
+    private InternalDependencyResolver(
+        TDependency instance)
         =>
         (this.instance, tag) = (instance, InternalDependencyResolverTag.Instance);
 
-    public InternalDependencyResolver(Func<TDependency> factory)
+    private InternalDependencyResolver(
+        Func<TDependency> factory)
         =>
         (this.factory, tag) = (factory, InternalDependencyResolverTag.Factory);
 
-    public InternalDependencyResolver(Func<IServiceProvider, TDependency> resolver)
+    private InternalDependencyResolver(
+        Func<IServiceProvider, TDependency> resolver)
         =>
         (this.resolver, tag) = (resolver, InternalDependencyResolverTag.Resolver);
+
+    public static implicit operator InternalDependencyResolver<TDependency>(
+        TDependency instance)
+        =>
+        new(instance);
+
+    public static implicit operator InternalDependencyResolver<TDependency>(
+        Func<TDependency> factory)
+        =>
+        new(factory);
+
+    public static implicit operator InternalDependencyResolver<TDependency>(
+        Func<IServiceProvider, TDependency> resolver)
+        =>
+        new(resolver);
 
     public TDependency Invoke(IServiceProvider serviceProvider)
         =>
@@ -33,16 +51,4 @@ internal sealed class InternalDependencyResolver<TDependency>
             InternalDependencyResolverTag.Resolver => resolver!.Invoke(serviceProvider),
             _ => throw new InvalidOperationException("An unexpected tag value.")
         };
-
-    public static implicit operator InternalDependencyResolver<TDependency>(TDependency instance)
-        =>
-        new(instance);
-
-    public static implicit operator InternalDependencyResolver<TDependency>(Func<TDependency> factory)
-        =>
-        new(factory);
-
-    public static implicit operator InternalDependencyResolver<TDependency>(Func<IServiceProvider, TDependency> resolver)
-        =>
-        new(resolver);
 }

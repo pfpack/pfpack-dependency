57f5bda3e7bedec8c7a189ced53a7597e4fddef6
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-03-20 17:04:02 +0300

    Arrange InternalDependencyResolver files

---
+++ src/dependency-core/Core/InternalDependencyResolver/InternalDependencyResolver.T.Factory.Implicit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimeFuncPack;
+
+partial class InternalDependencyResolver<TDependency>
+{
+    public static implicit operator InternalDependencyResolver<TDependency>(
+        TDependency instance)
+        =>
+        new(instance);
+
+    public static implicit operator InternalDependencyResolver<TDependency>(
+        Func<TDependency> factory)
+        =>
+        new(factory);
+
+    public static implicit operator InternalDependencyResolver<TDependency>(
+        Func<IServiceProvider, TDependency> resolver)
+        =>
+        new(resolver);
+}
--- src/dependency-core/Core/InternalDependencyResolver/InternalDependencyResolver.T.cs
+++ src/dependency-core/Core/InternalDependencyResolver/InternalDependencyResolver.T.cs
@@ -2,7 +2,7 @@
 
 namespace PrimeFuncPack;
 
-internal sealed class InternalDependencyResolver<TDependency>
+internal sealed partial class InternalDependencyResolver<TDependency>
 {
     private readonly InternalDependencyResolverTag tag;
 
@@ -11,53 +11,4 @@ internal sealed class InternalDependencyResolver<TDependency>
     private readonly Func<TDependency>? factory;
 
     private readonly Func<IServiceProvider, TDependency>? resolver;
-
-    public InternalDependencyResolver(
-        TDependency instance)
-        =>
-        (this.instance, tag) = (instance, InternalDependencyResolverTag.Instance);
-
-    public InternalDependencyResolver(
-        Func<TDependency> factory)
-        =>
-        (this.factory, tag) = (factory, InternalDependencyResolverTag.Factory);
-
-    public InternalDependencyResolver(
-        Func<IServiceProvider, TDependency> resolver)
-        =>
-        (this.resolver, tag) = (resolver, InternalDependencyResolverTag.Resolver);
-
-    public static implicit operator InternalDependencyResolver<TDependency>(
-        TDependency instance)
-        =>
-        new(instance);
-
-    public static implicit operator InternalDependencyResolver<TDependency>(
-        Func<TDependency> factory)
-        =>
-        new(factory);
-
-    public static implicit operator InternalDependencyResolver<TDependency>(
-        Func<IServiceProvider, TDependency> resolver)
-        =>
-        new(resolver);
-
-    public TDependency Invoke(IServiceProvider serviceProvider)
-        =>
-        tag switch
-        {
-            InternalDependencyResolverTag.Instance
-            =>
-            instance!,
-
-            InternalDependencyResolverTag.Factory
-            =>
-            factory!.Invoke(),
-
-            InternalDependencyResolverTag.Resolver
-            =>
-            resolver!.Invoke(serviceProvider),
-
-            _ => throw new InvalidOperationException("An unexpected tag value.")
-        };
 }

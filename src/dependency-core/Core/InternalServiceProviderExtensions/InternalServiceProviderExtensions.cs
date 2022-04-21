7789155c73c9796b91a1da21daefcd9a95260dc4
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-04-21 19:31:05 +0300

    Minor rename

--- src/dependency-core/Core/InternalServiceProviderExtensions/InternalServiceProviderExtensions.cs
+++ src/dependency-core/Core/InternalServiceProviderExtensions/InternalServiceProviderExtensions.cs
@@ -5,16 +5,16 @@ namespace PrimeFuncPack;
 internal static class InternalServiceProviderExtensions
 {
     internal static TResult InternalResolveThenMap<T, TResult>(
-        this IServiceProvider sp,
+        this IServiceProvider serviceProvider,
         Func<IServiceProvider, T> resolver,
         Func<T, TResult> map)
         =>
-        sp.InternalPipe(resolver).InternalPipe(map);
+        serviceProvider.InternalPipe(resolver).InternalPipe(map);
 
     internal static TResult InternalResolveThenMap<T, TResult>(
-        this IServiceProvider sp,
+        this IServiceProvider serviceProvider,
         Func<IServiceProvider, T> resolver,
         Func<IServiceProvider, T, TResult> map)
         =>
-        sp.InternalPipe(resolver).InternalPipe(dependency => map.Invoke(sp, dependency));
+        serviceProvider.InternalPipe(resolver).InternalPipe(dependency => map.Invoke(serviceProvider, dependency));
 }

71c3521f7fecfbf1874e4c4ba0e16089eb8ce7de
Pavel Moskovoy <p@moskovoi.ru>
2026-04-27 22:45:23 +0300

    Fix obsolete attribute message formatting

--- src/dependency-core/Core.Tests/Tests.Dependency/Empty/Empty.cs
+++ src/dependency-core/Core.Tests/Tests.Dependency/Empty/Empty.cs
@@ -6,7 +6,7 @@ namespace PrimeFuncPack.Tests;
 partial class DependencyTest
 {
     [Fact]
-    [Obsolete("This test is obsolete and will be removed in future versions")]
+    [Obsolete("This test is obsolete and will be removed in future versions.")]
     public void Empty_ExpectResolvedValueIsUnit()
     {
         var actual = Dependency.Empty();
@@ -16,7 +16,7 @@ partial class DependencyTest
     }
 
     [Fact]
-    [Obsolete("This test is obsolete and will be removed in future versions")]
+    [Obsolete("This test is obsolete and will be removed in future versions.")]
     public void EmptyTwoTimes_ExpectDependenciesAreNotSame()
     {
         var firstDependency = Dependency.Empty();

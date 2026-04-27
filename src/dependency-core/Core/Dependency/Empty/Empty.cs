04c46166119ab6548114a8e625523d64cb2f1f00
pmosk <pm@ggroupp.ru>
2026-04-27 22:38:08 +0300

    Set v2.2.0-rc.1 version. Mark Dependency.Empty method as obsolete.

--- src/dependency-core/Core/Dependency/Empty/Empty.cs
+++ src/dependency-core/Core/Dependency/Empty/Empty.cs
@@ -4,6 +4,7 @@ namespace PrimeFuncPack;
 
 partial class Dependency
 {
+    [Obsolete("This method is obsolete and will be removed in future versions")]
     public static Dependency<Unit> Empty()
         =>
         new(default(Unit));
